a1baa9cefcccf4413b1153865cff581eee59de3b
taty3385 <tatyy661@gmail.com>
2024-05-24 00:24:22 -0300

    agregp fetch de las top 10

--- src/hooks/useHome.js
+++ src/hooks/useHome.js
@@ -1,31 +1,34 @@
 import axios from "axios";
 import { useState } from "react";
-
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function useHome() {
   const [movies, setMovies] = useState([]);
   const [movieImages, setMovieImages] = useState({});
   const [totalPage, setTotalPage] = useState(0);
   const [page, setPage] = useState(1);
-  const apiKey= "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NjE3MzkyNTMwNmVjY2RhNGNkNzljY2FjMjYxMjk0ZiIsInN1YiI6IjY2M2MwZTQwYTFjN2FkN2Y5MTMzOGEzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GAh4z-ksZTZPe1aDUzRKA9V1jFbEVUTi6wxAysOXSXw"
+  const [top10, setTop10] = useState([]);
+  const apiKey =
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NjE3MzkyNTMwNmVjY2RhNGNkNzljY2FjMjYxMjk0ZiIsInN1YiI6IjY2M2MwZTQwYTFjN2FkN2Y5MTMzOGEzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GAh4z-ksZTZPe1aDUzRKA9V1jFbEVUTi6wxAysOXSXw";
 
   const getAllMovies = async (type) => {
+
     try {
       const response = await axios.get(
         `https://api.themoviedb.org/3/movie/${type}?language=es-ES&page=${page}`,
         {
           headers: {
             accept: "application/json",
-            Authorization: apiKey
-              
+            Authorization: apiKey,
           },
         }
       );
-
       setMovies(response.data.results);
       setTotalPage(response.data.total_pages);
-
+      setTop10(response.data.results)
+      
       const imagesData = {};
       for (const movie of response.data.results) {
         const imageResponse = await axios.get(
@@ -33,8 +36,7 @@ export default function useHome() {
           {
             headers: {
               accept: "application/json",
-              Authorization:apiKey
-                
+              Authorization: apiKey,
             },
           }
         );
@@ -49,8 +51,35 @@ export default function useHome() {
   const handleChange = (event, value) => {
     setPage(value);
   };
+
+  const getTop10 = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/top_rated?language=es-ES`,
+        {
+          headers: {
+            accept: "application/json",
+            Authorization: apiKey,
+          },
+        }
+      );
+      console.log(response.data.results);
+      setTop10(response.data.results);
+    } catch (error) {
+      console.error("Error fetching top 10 movies:", error);
+    }
+  };
  
-
-
-  return { getAllMovies, movies, movieImages, totalPage, page, handleChange,apiKey };
+  return {
+    getAllMovies,
+    movies,
+    movieImages,
+    totalPage,
+    page,
+    handleChange,
+    apiKey,
+    getTop10,
+    top10,
+  
+  };
 }
--- src/vievs/Home.jsx
+++ src/vievs/Home.jsx
@@ -1,35 +1,42 @@
-import React, { useEffect } from 'react';
-import Carrusel from './Carrusel';
-import useHome from '../hooks/useHome';
-import { Container, Grid, Typography } from '@mui/material';
-import MovieCard from '../components/MovieCard';
+import React, { useEffect } from "react";
+import Carrusel from "./Carrusel";
+import useHome from "../hooks/useHome";
+import { Container, Grid, Typography } from "@mui/material";
+import MovieCard from "../components/MovieCard";
 
 export default function Home() {
-  const { getAllMovies, movies } = useHome();
+  const { getAllMovies, movies, top10, getTop10 } = useHome();
 
   useEffect(() => {
-    getAllMovies('popular');
-    getAllMovies('top_rated');
+    getTop10();
+    getAllMovies("popular");
+  }, []);
+
+  useEffect(() => {
+    getTop10();
   }, []);
 
   return (
     <div>
-      <Carrusel 
-      autoPlay={false}
-      infiniteLoop={true}
-      interval={3000}
-      showStatus={false}
-      showThumbs={false}/>
+      <Carrusel
+        autoPlay={false}
+        infiniteLoop={true}
+        interval={3000}
+        showStatus={false}
+        showThumbs={false}
+      />
       <Container>
         <Typography variant="h4" component="h2" gutterBottom>
           Lo más visto
         </Typography>
         <Grid container spacing={3}>
-          {movies && movies.length > 0 && movies.map((movie) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={movie.id}>
-              <MovieCard movie={movie} />
-            </Grid>
-          ))}
+          {movies &&
+            movies.length > 0 &&
+            movies.map((movie) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={movie.id}>
+                <MovieCard movie={movie} />
+              </Grid>
+            ))}
         </Grid>
       </Container>
       <Container>
@@ -37,11 +44,13 @@ export default function Home() {
           Top 10
         </Typography>
         <Grid container spacing={3}>
-          {movies && movies.length > 0 && movies.map((movie) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={movie.id}>
-              <MovieCard movie={movie} />
-            </Grid>
-          ))}
+          {top10 &&
+            top10.length > 0 &&
+            top10.map((movie) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={movie.id}>
+                <MovieCard movie={movie} />
+              </Grid>
+            ))}
         </Grid>
       </Container>
     </div>
